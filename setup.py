1ffae1f38f835ba870765384bca24ddd9170de4a
Erik van den Brink <git@erikvandenbrink.com>
2022-08-08 10:51:01 +0200

    Update readme/setup.py

--- setup.py
+++ setup.py
@@ -99,7 +99,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
